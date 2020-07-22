1ff12747e40391dc838ee713abe6782f4739e4ce
badat <you@example.com>
2020-07-22 08:30:56 +0700

    Test List Property

---
+++ src/app/service/property.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, Subject} from 'rxjs';
+import {IProperty} from '../interface/iproperty';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PropertyService {
+  // tslint:disable-next-line:variable-name
+  private readonly API_URL = 'http://localhost:8080/api/property';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getListProperty(): Observable<IProperty[]> {
+    return this.httpClient.get<IProperty[]>(this.API_URL);
+  }
+}
