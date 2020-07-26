fa04ddab84433807a3e18e20a41b21dff62d2c7a
thinhbui2502 <buiducthinh2502@gmail.com>
2020-07-26 11:08:35 +0700

    start property detail with some errors

--- src/app/component/property-detail/property-detail.component.ts
+++ src/app/component/property-detail/property-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {IProperty} from '../../interface/iproperty';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PropertyService} from '../../service/property.service';
 
 @Component({
   selector: 'app-property-detail',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-detail.component.css']
 })
 export class PropertyDetailComponent implements OnInit {
+  propertyId: number;
+  propertyForm: FormGroup;
+  property: IProperty;
 
-  constructor() { }
+  constructor(private propertyService: PropertyService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.propertyId = params.id;
+      this.propertyService.getPropertyById(this.propertyId).subscribe(result => {
+        // this.bookForm.setValue(result);
+        this.property = result;
+      });
+    });
   }
 
 }
