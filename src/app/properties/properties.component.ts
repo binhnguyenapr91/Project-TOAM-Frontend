7cacb063f31f0a1e54f6af272b0793a66090934f
badat <you@example.com>
2020-07-26 16:52:42 +0700

    delete property

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -21,6 +21,21 @@ export class PropertiesComponent implements OnInit {
     }, error => {
       this.properties = [];
     });
+    this.propertyService.shouldRefresh.subscribe(result => {
+      this.propertyService.getListProperty().subscribe(results => {
+        this.properties = results;
+        console.log(results);
+      }, error => {
+        this.properties = [];
+      });
+    });
   }
 
+  deleteProperty(id: number): void {
+    if (confirm('I want to delete ?')) {
+      this.propertyService.deleteProperty(id).subscribe(result => {
+        this.propertyService.shouldRefresh.next();
+      });
+    }
+  }
 }
--- src/app/service/property.service.ts
+++ src/app/service/property.service.ts
@@ -9,7 +9,7 @@ import {IProperty} from '../interface/iproperty';
 export class PropertyService {
   // tslint:disable-next-line:variable-name
   private readonly API_URL = 'http://localhost:8080/api/property';
-
+  shouldRefresh = new Subject<any>();
   constructor(private httpClient: HttpClient) {
   }
 
@@ -19,5 +19,8 @@ export class PropertyService {
 
   createProperty(property: IProperty): Observable<IProperty> {
     return this.httpClient.post<IProperty>(this.API_URL, property);
+  }
+  deleteProperty(id: number): Observable<any> {
+    return this.httpClient.delete(`${this.API_URL}/${id}`);
   }
 }
