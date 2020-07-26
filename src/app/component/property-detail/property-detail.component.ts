5cb2511b6a441ddb680f15ead948ad42921d6ebc
thinhbui2502 <buiducthinh2502@gmail.com>
2020-07-26 18:02:14 +0700

    add random property in detail property

--- src/app/component/property-detail/property-detail.component.ts
+++ src/app/component/property-detail/property-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {IProperty} from '../../interface/iproperty';
 import {ActivatedRoute, Router} from '@angular/router';
@@ -13,18 +13,27 @@ export class PropertyDetailComponent implements OnInit {
   propertyId: number;
   propertyForm: FormGroup;
   property: IProperty;
+  // khai biến để lấy random property
+  randomPropertyId: number;
+  randomProperty: IProperty;
 
   constructor(private propertyService: PropertyService,
               private activatedRoute: ActivatedRoute,
-              private router: Router) { }
+              private router: Router) {
+  }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
+      // lấy về property theo id
       this.propertyId = params.id;
       this.propertyService.getPropertyById(this.propertyId).subscribe(result => {
         this.property = result;
       });
+      // this.randomPropertyId = params.id + Math.round(Math.random() * this.properties.length);
+      this.randomPropertyId = Math.ceil(params.id * Math.random());
+      this.propertyService.getPropertyById(this.randomPropertyId).subscribe(result => {
+        this.randomProperty = result;
+      });
     });
   }
-
 }
