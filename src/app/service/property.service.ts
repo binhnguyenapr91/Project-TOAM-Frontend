a8f912d8e8baf560a1f8fed352a4e5121a599cd5
badat <you@example.com>
2020-07-27 23:50:39 +0700

    query find theo loai nha

--- src/app/service/property.service.ts
+++ src/app/service/property.service.ts
@@ -9,7 +9,9 @@ import {IProperty} from '../interface/iproperty';
 export class PropertyService {
   // tslint:disable-next-line:variable-name
   private readonly API_URL = 'http://localhost:8080/api/property';
+  private readonly API = 'http://localhost:8080/api/property/type';
   shouldRefresh = new Subject<any>();
+
   constructor(private httpClient: HttpClient) {
   }
 
@@ -20,7 +22,12 @@ export class PropertyService {
   createProperty(property: IProperty): Observable<IProperty> {
     return this.httpClient.post<IProperty>(this.API_URL, property);
   }
+
   deleteProperty(id: number): Observable<any> {
     return this.httpClient.delete(`${this.API_URL}/${id}`);
+  }
+
+  findPropertyByPropertyType(name: string): Observable<IProperty[]> {
+    return this.httpClient.get<IProperty[]>(`${this.API}/${name}`);
   }
 }
