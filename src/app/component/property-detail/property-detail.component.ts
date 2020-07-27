b8d26c43bc86bd56d20d4e56a0066d49148d89bd
thinhbui2502 <buiducthinh2502@gmail.com>
2020-07-27 23:14:59 +0700

    add url safe to show iframe demo

--- src/app/component/property-detail/property-detail.component.ts
+++ src/app/component/property-detail/property-detail.component.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit, Pipe, PipeTransform} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {IProperty} from '../../interface/iproperty';
 import {ActivatedRoute, Router} from '@angular/router';
 import {PropertyService} from '../../service/property.service';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-property-detail',
   templateUrl: './property-detail.component.html',
   styleUrls: ['./property-detail.component.css']
 })
+
 export class PropertyDetailComponent implements OnInit {
   propertyId: number;
   propertyForm: FormGroup;
@@ -16,11 +18,16 @@ export class PropertyDetailComponent implements OnInit {
   // khai biến để lấy next property
   nextPropertyId: number;
   nextProperty: IProperty;
+  //
+  name = 'Set iframe source';
+  url: string = "https://www.google.com/maps?q=200 van cao&output=embed";
+  urlSafe: SafeResourceUrl;
 
 
   constructor(private propertyService: PropertyService,
               private activatedRoute: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              public sanitizer: DomSanitizer ) {
   }
 
   ngOnInit(): void {
@@ -35,6 +42,8 @@ export class PropertyDetailComponent implements OnInit {
       this.propertyService.getPropertyById(this.nextPropertyId).subscribe(result => {
         this.nextProperty = result;
       });
+      //
+      this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
     });
   }
 }
