6b3145c5adab62baaa63b87bf1cde57434b6b87d
badat <you@example.com>
2020-07-28 09:41:38 +0700

    fix git

--- src/app/service/property.service.ts
+++ src/app/service/property.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable, Subject} from 'rxjs';
 import {IProperty} from '../interface/iproperty';
+import {IPropertyType} from '../interface/IPropertyType';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,14 @@ export class PropertyService {
   // tslint:disable-next-line:variable-name
   private readonly API_URL = 'http://localhost:8080/api/property';
   private readonly API = 'http://localhost:8080/api/property/type';
+  private readonly API_ = 'http://localhost:8080/api/propertiesType';
   shouldRefresh = new Subject<any>();
 
   constructor(private httpClient: HttpClient) {
+  }
+
+  getListPropertyType(): Observable<IPropertyType[]> {
+    return this.httpClient.get<IPropertyType[]>(this.API_);
   }
 
   getListProperty(): Observable<IProperty[]> {
