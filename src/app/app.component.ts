29df56fb05254c90991d182c0525a9f75c92ea69
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-28 14:02:06 +0700

    fix reload Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {TokenStorageService} from './_services/token-storage.service';
+import {UserService} from './_services/user.service';
+import {Subject} from 'rxjs';
+import {AuthService} from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +16,25 @@ export class AppComponent implements OnInit{
   showRenterBoard = false;
   username: string;
 
-  constructor(private tokenStorageService: TokenStorageService) { }
+  constructor(private tokenStorageService: TokenStorageService,
+              private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.getNavigate();
+    this.authService.shouldRefresh.subscribe(req => {
+      this.getNavigate();
+    });
+  }
+  getNavigate(): any{
     this.isLoggedIn = !!this.tokenStorageService.getToken();
 
     if (this.isLoggedIn) {
       const user = this.tokenStorageService.getUser();
       this.roles = user.roles;
-
       this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
       this.showHostBoard = this.roles.includes('ROLE_HOST');
       this.showRenterBoard = this.roles.includes('ROLE_RENTER');
-
       this.username = user.username;
     }
   }
