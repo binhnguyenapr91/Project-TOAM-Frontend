c4bf8d89396d0137a508dece50391b7e38f63403
badat <you@example.com>
2020-07-29 08:21:07 +0700

    xem thong tin chi tiet renter

---
+++ src/app/component/accountRenter-list/renter-list/renter-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RenterListComponent } from './renter-list.component';
+
+describe('RenterListComponent', () => {
+  let component: RenterListComponent;
+  let fixture: ComponentFixture<RenterListComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RenterListComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RenterListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
