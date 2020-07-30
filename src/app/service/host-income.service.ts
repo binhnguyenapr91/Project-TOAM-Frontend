603a707ec1e83fe98d527ed4e9771cd9f29818fb
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-31 01:16:59 +0700

    add quantity of contracts

--- src/app/service/host-income.service.ts
+++ src/app/service/host-income.service.ts
@@ -10,16 +10,22 @@ import {IHostIncome} from '../interface/IHostIncome';
 export class HostIncomeService {
   private readonly API_URL = 'http://localhost:8080/api/host/value';
   private readonly API_URL1 = 'http://localhost:8080/api/host/allValue';
+  private readonly API_URL2 = 'http://localhost:8080/api/host/contractsQuantity';
   shouldRefresh = new Subject<any>();
 
   constructor(private httpClient: HttpClient) {
   }
 
   getHostIncomeList(): Observable<IHostIncome[]> {
-    return this.httpClient.get<IHostIncome[]>(this.API_URL);
+    // return this.httpClient.get<IHostIncome[]>(this.API_URL);
+    return this.httpClient.post<IHostIncome[]>(this.API_URL, HttpClient);
   }
 
   getAmount(): Observable<number> {
-    return this.httpClient.get<number>(this.API_URL1);
+    return this.httpClient.post<number>(this.API_URL1, HttpClient);
+  }
+
+  getAllContractsQuan(): Observable<number> {
+    return this.httpClient.post<number>(this.API_URL2, HttpClient);
   }
 }
